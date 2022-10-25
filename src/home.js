ca5b241f77f0ba144796850a3d12b0dec22ef032
ausvat <andrewusvat7@gmail.com>
2022-10-25 14:17:41 -0700

    new home page ,sign up page

--- src/home.js
+++ src/home.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './styles/home.css'
 
 export function Home() {
     return(
-        <div>
-            <h1>
-               Home Page 
+        <div className = "page">
+            <div className = "main-home-card">
+            <h1 className = "titleAlign">
+               Welcome Home!<br></br> Here are some freshly baked jokes 
             </h1>
+            </div>
         </div>
     );
 }
