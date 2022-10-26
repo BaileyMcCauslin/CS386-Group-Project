cd35ec13fc901717e54fa9b54b70a7ee692d2474
Cody Beck <cjb873@nau.edu>
2022-10-26 11:02:53 -0700

    Add saved model, test data, and prediction script

---
+++ predictions.py
@@ -0,0 +1,45 @@
+
+import pandas as pd
+import pickle
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+import json
+import argparse
+
+
+def main():
+    loaded_kmeans = pickle.load(open('model.sav', 'rb'))
+    loaded_df = pickle.load(open('data.sav', 'rb'))
+    input_json = None
+
+    mapping = {0: 'no', 1: 'dancing', 2: 'funny', 3: 'crying', 4: 'hello'}
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-j', required=True, help='give json file')
+    args = parser.parse_args()
+
+    with open(args.j, 'r+', encoding='utf-8') as json_file:
+        for line in json_file:
+            input_json = json.loads(line)
+
+    input_json = {k: [v] for k, v in input_json.items()}
+    input_df = pd.DataFrame(input_json)
+    loaded_df = pd.concat([loaded_df, input_df], ignore_index=True)
+
+    cat_features = ["Music", "Game", "Activity", "Food", "Movie"]
+    one_hot = OneHotEncoder()
+
+    transformer = ColumnTransformer([("one_hot", one_hot, cat_features)],
+                                    remainder="passthrough",
+                                    sparse_threshold=0)
+
+    features_transformed = transformer.fit_transform(loaded_df)
+
+    input_df = pd.DataFrame(features_transformed).tail(1)
+
+    prediction = mapping.get(loaded_kmeans.predict(input_df)[0])
+
+    with open('search.json', 'w', encoding='utf-8') as outfile:
+        outfile.write('{\"search_value\": \"' + prediction + '\"}')
+
+if __name__ == '__main__':
+    main()
