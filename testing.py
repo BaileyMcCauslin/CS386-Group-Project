a4cb0dbca99b8ee4aacee42216e1e87c6a59d510
cjb873 <cjb873@nau.edu>
2022-10-30 18:58:36 -0700

    OOPify

--- testing.py
+++ testing.py
@@ -1,21 +1,25 @@
 import unittest
 import pandas as pd
 import pickle
-import predictions as p
+from predictions import Prediction
 
 
 class TestPredictions(unittest.TestCase):
 
+
     def test_json_to_df(self):
+        p = Prediction()
         data = ['folk', 'risk', 'watching', 'apple', 'drama']
         df = pd.DataFrame(data).T
         self.assertEqual(df.values.tolist(), p.json_to_df().values.tolist())
 
     def test_prediction(self):
+        p = Prediction()
         in_df = pickle.load(open('input_df.sav', 'rb'))
         self.assertEqual('crying', p.get_prediction(in_df))
 
     def test_convert(self):
+        p = Prediction()
         loaded_df = pickle.load(open('loaded_df.sav', 'rb'))
         data = [1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  1.0,  0.0,  0.0,
                 0.0,  0.0,  1.0,  1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,
@@ -26,6 +30,7 @@ class TestPredictions(unittest.TestCase):
                          p.convert_one_hot(loaded_df).values.tolist())
 
     def test_transform(self):
+        p = Prediction()
         self.assertEqual('no', p.transform_with_map([0]))
 
 
